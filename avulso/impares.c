6e990b563b656949ca0495fc512125392e85706b
guiness333 <walber123k@hotmail.com>
2016-09-15 13:20:26 -0300

    Arrumando

--- avulso/impares.c
+++ avulso/impares.c
@@ -2,15 +2,15 @@
 
 void main(){
 
-    int n,qI=0,i;
+    int n,qI=1,i;
     printf("Quantos numeros impares voce quer ver?:\n");
     scanf("%d",&n);
-    for (i = 1; i <= n*2; i++) {
-        if(i%2==0){
-        }else{
+    printf("O numero impar e :::::%d° : %d:::::\n",1,1);
+    for (i = 2; i <= n; i++) {
             qI++;
-            printf("O numero e impar :::::%d° : %d:::::\n",qI,i);
-        }
+                printf("O numero e impar :::::%d° : %d:::::\n",qI,2*i+1);
+            
+        
     }
 
 }
